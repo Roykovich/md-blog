f48923124cffa5156e4c416b231ff535f67a4532
Rubén Ortiz <runerd.menethil@gmail.com>
2022-03-06 23:42:32 -0300

    Create stylesheet for ButtonHandler component

--- src/components/NewPost/ButtonHandler.js
+++ src/components/NewPost/ButtonHandler.js
@@ -1,8 +1,10 @@
+import "./ButtonHandler.css";
+
 const ButtonHandler = () => {
   return (
     <div id="mobile-buttons-handler">
-      <button>Editor</button>
-      <button>Preview</button>
+      <button className="mobile-buttons active">Editor</button>
+      <button className="mobile-buttons">Preview</button>
     </div>
   );
 };
